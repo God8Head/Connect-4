a94474de6d9e8b1389e2a4bb8ca9e2a266ecf9cf
jaime <jaime.acosta3@utp.ac.pa>
2022-06-24 21:57:06 -0500

    Work smarter, not harder.

--- scripts/game.js
+++ scripts/game.js
@@ -34,14 +34,34 @@ let time                  = 0
 let movimientos           = 0
 let EnJuego               = true
 
-let tablero_interfaz = [
+/*let tablero_interfaz = [
   [document.getElementById("a1"), document.getElementById("a2"), document.getElementById("a3"), document.getElementById("a4"), document.getElementById("a5"), document.getElementById("a6"), document.getElementById("a7")],
   [document.getElementById("b1"), document.getElementById("b2"), document.getElementById("b3"), document.getElementById("b4"), document.getElementById("b5"), document.getElementById("b6"), document.getElementById("b7")],
   [document.getElementById("c1"), document.getElementById("c2"), document.getElementById("c3"), document.getElementById("c4"), document.getElementById("c5"), document.getElementById("c6"), document.getElementById("c7")],
   [document.getElementById("d1"), document.getElementById("d2"), document.getElementById("d3"), document.getElementById("d4"), document.getElementById("d5"), document.getElementById("d6"), document.getElementById("d7")],
   [document.getElementById("e1"), document.getElementById("e2"), document.getElementById("e3"), document.getElementById("e4"), document.getElementById("e5"), document.getElementById("e6"), document.getElementById("e7")],
   [document.getElementById("f1"), document.getElementById("f2"), document.getElementById("f3"), document.getElementById("f4"), document.getElementById("f5"), document.getElementById("f6"), document.getElementById("f7")],
-]
+]*/
+
+const CargarTablero_interfaz = () => {
+  let output = Array(Array())
+
+  console.log(output.length)
+  output.pop()
+
+  for (let i = 0; i <= 5; i++) {
+    let rows = Array()
+    for (let j = 1; j <= 7; j++) {
+      // El código 97 es el caracter a en ascii.
+      rows.push(document.getElementById(`${String.fromCharCode(97 + i)}${j}`))
+    }
+
+    output.push(rows)
+  }
+  return output
+}
+
+tablero_interfaz = CargarTablero_interfaz()
 
 let tablero = [
   ["", "", "", "", "", "", ""],
